e140368ebac2b1fadaf584ed1e3a18c07a570ce2
Saul Belisle <rbelisle1270@gmail.com>
2020-07-29 12:01:57 -0700

    adding flight and airport models

---
+++ db/migrate/20200729185909_change_time_to_integer.rb
@@ -0,0 +1,5 @@
+class ChangeTimeToInteger < ActiveRecord::Migration[5.2]
+  def change
+    change_column :flights, :duration, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Airport.destroy_all
+Airport.reset_pk_sequence
+
+airports = Airport.create([{code: "SFO"}, {code: "NYC"}, {code: "LAS"}])
+
+Flight.destroy_all
+Flight.reset_pk_sequence
+
+flights = Flight.create([
+    {from_airport_id: 1, to_airport_id: 2, departure: DateTime.new(2020, 10, 1, 10, 0, 0), duration: 6},
+    {from_airport_id: 1, to_airport_id: 3, departure: DateTime.new(2020, 11, 2, 10, 0, 0), duration: 8},
+    {from_airport_id: 2, to_airport_id: 1, departure: DateTime.new(2020, 12, 3, 10, 0, 0), duration: 6.5},
+    {from_airport_id: 2, to_airport_id: 3, departure: DateTime.new(2020, 10, 4, 10, 0, 0), duration: 7},
+    {from_airport_id: 3, to_airport_id: 1, departure: DateTime.new(2020, 12, 5, 10, 0, 0), duration: 9},
+    {from_airport_id: 3, to_airport_id: 2, departure: DateTime.new(2020, 11, 7, 10, 0, 0), duration: 7}
+    ])
