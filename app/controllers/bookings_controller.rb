be84a594855029bdeb27fe0ff97af4afb0030f6c
Saul Belisle <rbelisle1270@gmail.com>
2020-09-05 13:23:51 -0700

    nested form finished

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,31 @@
+class BookingsController < ApplicationController
+
+    def new
+        @flight = Flight.find(params[:flight_id])
+        @booking = Booking.new
+        @passenger = Passenger.new
+        params[:num_passengers].to_i.times { @booking.passengers.build }
+    end
+    
+    def create
+        @booking = Booking.new(booking_params)
+        if @booking.save
+            redirect_to booking_path(@booking)
+        else
+            render new_booking_path
+        end
+    end
+    
+    def show
+        @booking = Booking.find(params[:id])
+        @flight = @booking.flight
+    end
+    
+    private
+    
+    def booking_params
+        params.require(:booking).permit(:flight_id, :num_passengers,
+        passengers_attributes: [:id, :name, :email])
+    end
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_185909) do
+ActiveRecord::Schema.define(version: 2020_09_05_201026) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "num_passengers"
+    t.integer "flight_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -25,6 +33,15 @@ ActiveRecord::Schema.define(version: 2020_07_29_185909) do
     t.datetime "updated_at", null: false
     t.integer "to_airport_id"
     t.integer "from_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
 end
