f99d128de147ef897b586a5dc59c141b62050476
Saul Belisle <rbelisle1270@gmail.com>
2020-09-05 12:59:00 -0700

    simple form for flight search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,26 +2,14 @@ class FlightsController < ApplicationController
 
     def index
         @flights = Flight.all
+        @airports_array = Airport.all.map { |airport| [airport.code, airport.id] }
+        @months_array = Flight.all.map { |flight| flight.departure.month }.uniq.sort
+        @days_array = Flight.all.map { |flight| flight.departure.day }.uniq.sort
+
+        @available_flights = @flights.where(from_airport_id: params[:from_airport_id])
+                                    .where(to_airport_id: params[:to_airport_id])
+                                    .search_by_month(params[:month])
+                                    .search_by_day(params[:day])
     end
-
-    def new
-        @flight = Flight.new
-    end
-
-    def show
-        @flight = Flight.find(params[:id])
-    end
-
-    def edit
-        @flight = Flight.find(params[:id])
-    end
-
-
-
-  private
-
-#   def whitelisted_post_params
-#       params.require(:flight).permit(:description,:location, :date, :creator, :creator_id, :attendees)
-#   end
 
 end
